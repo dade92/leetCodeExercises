78346ca6593abefc40e661f4e505b2d019e83bff
dbotti <davide.botti@lastminute.com>
2024-10-11 14:28:59 +0200

    Refactor

--- src/main/java/com/myprojects/lists/Utils.java
+++ src/main/java/com/myprojects/lists/Utils.java
@@ -99,12 +99,4 @@ final class Utils {
         return head;
     }
 
-    public static void printList(ListNode head) {
-        while (head != null) {
-            System.out.print(head.val + " ");
-            head = head.next;
-        }
-        System.out.println();
-    }
-
 }
