3121e9ab2b03955bbee1addeb48d06a1f661d1a6
dbotti <davide.botti@lastminute.com>
2024-10-11 11:10:35 +0200

    Test refactor

--- src/main/java/com/myprojects/App.java
+++ src/main/java/com/myprojects/App.java
@@ -3,12 +3,8 @@ package com.myprojects;
 import com.myprojects.lists.ListNode;
 import com.myprojects.lists.Utils;
 
-/**
- * Hello world!
- *
- */
 public class App {
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
         ListNode head = new ListNode(1);
 
         head = Utils.addElement(head, 2, 2);
