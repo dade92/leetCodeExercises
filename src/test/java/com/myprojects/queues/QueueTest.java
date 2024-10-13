866fa440900e72ffe8a2c5e8e43ae914fb5540ba
Davide Botti <davidebotti92@gmail.com>
2024-10-13 10:13:14 +0200

    Adds equals() to the List class

--- src/test/java/com/myprojects/queues/QueueTest.java
+++ src/test/java/com/myprojects/queues/QueueTest.java
@@ -2,8 +2,7 @@ package com.myprojects.queues;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class QueueTest {
 
@@ -50,5 +49,16 @@ public class QueueTest {
             new int[]{5, 8, 10},
             queue.printQueue()
         );
+    }
+
+    @Test
+    void equals() {
+        Queue another = new Queue(5, 8, 10);
+        Queue notEqual = new Queue(5, 8, 11);
+        Queue notEqual2 = new Queue(5, 8);
+
+        assertEquals(queue, another);
+        assertNotEquals(queue, notEqual);
+        assertNotEquals(queue, notEqual2);
     }
 }
