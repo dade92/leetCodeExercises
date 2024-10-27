e9dd50e06a8fd44d5092a87b467cd6a9f56932ad
Davide Botti <davidebotti92@gmail.com>
2024-10-27 11:32:35 +0100

    Implements hashTable basic functions

--- src/test/java/com/myprojects/hashtables/HashTableTest.java
+++ src/test/java/com/myprojects/hashtables/HashTableTest.java
@@ -1,10 +1,9 @@
 package com.myprojects.hashtables;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HashTableTest {
 
