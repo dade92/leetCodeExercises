90609da4214e7cc8837985cef40841d724e3561d
dbotti <davide.botti@lastminute.com>
2024-11-14 21:16:21 +0100

    BitSets

---
+++ src/main/java/com/myprojects/bitsets/BitSet.java
@@ -0,0 +1,48 @@
+package com.myprojects.bitsets;
+
+import java.util.StringJoiner;
+
+public class BitSet {
+    private final int[] bitArray;
+    private final int size;
+
+    public BitSet(int size) {
+        this.size = size;
+        bitArray = new int[(size + 31) / 32];
+    }
+
+    public void set(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        int arrayIndex = index / 32;
+        int bitPosition = index % 32;
+        bitArray[arrayIndex] |= (1 << bitPosition);
+    }
+
+    public boolean get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        int arrayIndex = index / 32;
+        int bitPosition = index % 32;
+        return (bitArray[arrayIndex] & (1 << bitPosition)) != 0;
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner sj = new StringJoiner("", "[", "]");
+        for (int i = 0; i < size; i++) {
+            sj.add(get(i) ? "1" : "0");
+        }
+        return sj.toString();
+    }
+
+    public void printBits() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(get(i) ? "1" : "0");
+        }
+        System.out.println();
+    }
+}
+
---
+++ src/test/java/com/myprojects/bitsets/BitSetTest.java
@@ -0,0 +1,24 @@
+package com.myprojects.bitsets;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class BitSetTest {
+
+    private final BitSet bitSet = new BitSet(10);
+
+    @Test
+    void getASetValue() {
+        bitSet.set(5);
+        assertTrue(bitSet.get(5));
+    }
+
+    @Test
+    void stringRepresentation() {
+        bitSet.set(3);
+
+        Assertions.assertEquals("[0001000000]", bitSet.toString());
+    }
+}
